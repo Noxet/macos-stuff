55061ef049712a675880e2b6d43d999b21045969
Jonathan Sönnerup <Nooxet@gmail.com>
2021-06-22 11:27:12 +0200

    Added some good tools to have

---
+++ README.md
@@ -0,0 +1,10 @@
+# MacOS tools
+
+A collection of tools n' stuff for mac
+
+## Install
+
+- Homebrew
+- oh-my-zsh
+- powerlevel10k theme for zsh
+
---
+++ good-tools.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+check_brew=`bre > /dev/null 2>&1`
+if $check_brew ; then
+	echo "Install homebrew first"
+	exit
+fi
+
+brew install iterm2
+brew install pyenv
